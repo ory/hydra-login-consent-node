f2dadec97f7b9905590053482f2119cc0e854a89
arekkas <aeneas@ory.am>
2018-05-24 12:58:28 +0200

    Makes session data from remember session optional

--- routes/consent.js
+++ routes/consent.js
@@ -32,10 +32,10 @@ router.get('/', csrfProtection, function (req, res, next) {
           session: {
             // This data will be available when introspecting the token. Try to avoid sensitive information here,
             // unless you limit who can introspect tokens.
-            access_token: { foo: 'bar' },
+            // access_token: { foo: 'bar' },
 
             // This data will be available in the ID token.
-            id_token: { baz: 'bar' },
+            // id_token: { baz: 'bar' },
           }
         }).then(function (response) {
           // All we need to do now is to redirect the user back to hydra!
