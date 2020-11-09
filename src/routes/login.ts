0cd9e8a0e56e65458883d5a7a97812de4cdd67fa
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-09 12:02:39 +0100

    feat: add oidc conformity stubs

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -2,25 +2,27 @@ import express from 'express'
 import url from 'url'
 import urljoin from 'url-join'
 import csrf from 'csurf'
-import {hydraAdmin} from '../config'
+import { hydraAdmin } from '../config'
+import { oidcConformityMaybeFakeAcr } from './stub/oidc-cert'
 
 // Sets up csrf protection
-const csrfProtection = csrf({cookie: true});
-const router = express.Router();
+const csrfProtection = csrf({ cookie: true })
+const router = express.Router()
 
 router.get('/', csrfProtection, (req, res, next) => {
   // Parses the URL query
-  const query = url.parse(req.url, true).query;
+  const query = url.parse(req.url, true).query
 
   // The challenge is used to fetch information about the login request from ORY Hydra.
-  const challenge = String(query.login_challenge);
+  const challenge = String(query.login_challenge)
   if (!challenge) {
     next(new Error('Expected a login challenge to be set but received none.'))
     return
   }
 
-  hydraAdmin.getLoginRequest(challenge)
-    .then(({body}) => {
+  hydraAdmin
+    .getLoginRequest(challenge)
+    .then(({ body }) => {
       // If hydra was already able to authenticate the user, skip will be true and we do not need to re-authenticate
       // the user.
       if (body.skip) {
@@ -29,13 +31,15 @@ router.get('/', csrfProtection, (req, res, next) => {
 
         // Now it's time to grant the login request. You could also deny the request if something went terribly wrong
         // (e.g. your arch-enemy logging in...)
-        return hydraAdmin.acceptLoginRequest(challenge, {
-          // All we need to do is to confirm that we indeed want to log in the user.
-          subject: String(body.subject)
-        }).then(({body}) => {
-          // All we need to do now is to redirect the user back to hydra!
-          res.redirect(String(body.redirectTo));
-        });
+        return hydraAdmin
+          .acceptLoginRequest(challenge, {
+            // All we need to do is to confirm that we indeed want to log in the user.
+            subject: String(body.subject)
+          })
+          .then(({ body }) => {
+            // All we need to do now is to redirect the user back to hydra!
+            res.redirect(String(body.redirectTo))
+          })
       }
 
       // If authentication can't be skipped we MUST show the login UI.
@@ -43,28 +47,33 @@ router.get('/', csrfProtection, (req, res, next) => {
         csrfToken: req.csrfToken(),
         challenge: challenge,
         action: urljoin(process.env.BASE_URL || '', '/login'),
-      });
+        hint: body.oidcContext?.loginHint || ''
+      })
     })
     // This will handle any error that happens when making HTTP calls to hydra
-    .catch(next);
-});
+    .catch(next)
+})
 
 router.post('/', csrfProtection, (req, res, next) => {
   // The challenge is now a hidden input field, so let's take it from the request body instead
-  const challenge = req.body.challenge;
+  const challenge = req.body.challenge
 
   // Let's see if the user decided to accept or reject the consent request..
   if (req.body.submit === 'Deny access') {
     // Looks like the consent request was denied by the user
-    return hydraAdmin.rejectLoginRequest(challenge, {
-      error: 'access_denied',
-      errorDescription: 'The resource owner denied the request'
-    }).then(({body})=> {
-      // All we need to do now is to redirect the browser back to hydra!
-      res.redirect(String(body.redirectTo));
-    })
-      // This will handle any error that happens when making HTTP calls to hydra
-      .catch(next);
+    return (
+      hydraAdmin
+        .rejectLoginRequest(challenge, {
+          error: 'access_denied',
+          errorDescription: 'The resource owner denied the request'
+        })
+        .then(({ body }) => {
+          // All we need to do now is to redirect the browser back to hydra!
+          res.redirect(String(body.redirectTo))
+        })
+        // This will handle any error that happens when making HTTP calls to hydra
+        .catch(next)
+    )
   }
 
   // Let's check if the user provided valid credentials. Of course, you'd use a database or some third-party service
@@ -76,33 +85,47 @@ router.post('/', csrfProtection, (req, res, next) => {
       csrfToken: req.csrfToken(),
       challenge: challenge,
       error: 'The username / password combination is not correct'
-    });
+    })
 
     return
   }
 
   // Seems like the user authenticated! Let's tell hydra...
-  hydraAdmin.acceptLoginRequest(challenge, {
-    // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
-    subject: 'foo@bar.com',
 
-    // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
-    // set the "skip" parameter in the other route to true on subsequent requests!
-    remember: Boolean(req.body.remember),
+  hydraAdmin
+    .getLoginRequest(challenge)
+    .then(({ body: loginRequest }) =>
+      hydraAdmin
+        .acceptLoginRequest(challenge, {
+          // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
+          subject: 'foo@bar.com',
 
-    // When the session expires, in seconds. Set this to 0 so it will never expire.
-    rememberFor: 3600,
+          // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
+          // set the "skip" parameter in the other route to true on subsequent requests!
+          remember: Boolean(req.body.remember),
 
-    // Sets which "level" (e.g. 2-factor authentication) of authentication the user has. The value is really arbitrary
-    // and optional. In the context of OpenID Connect, a value of 0 indicates the lowest authorization level.
-    // acr: '0',
-  })
-    .then( ({body})=> {
-      // All we need to do now is to redirect the user back to hydra!
-      res.redirect(String(body.redirectTo));
-    })
+          // When the session expires, in seconds. Set this to 0 so it will never expire.
+          rememberFor: 3600,
+
+          // Sets which "level" (e.g. 2-factor authentication) of authentication the user has. The value is really arbitrary
+          // and optional. In the context of OpenID Connect, a value of 0 indicates the lowest authorization level.
+          // acr: '0',
+          //
+          // If the environment variable CONFORMITY_FAKE_CLAIMS is set we are assuming that
+          // the app is built for the automated OpenID Connect Conformity Test Suite. You
+          // can peak inside the code for some ideas, but be aware that all data is fake
+          // and this only exists to fake a login system which works in accordance to OpenID Connect.
+          //
+          // If that variable is not set, the ACR value will be set to the default passed here ('0')
+          acr: oidcConformityMaybeFakeAcr(loginRequest, '0')
+        })
+        .then(({ body }) => {
+          // All we need to do now is to redirect the user back to hydra!
+          res.redirect(String(body.redirectTo))
+        })
+    )
     // This will handle any error that happens when making HTTP calls to hydra
-    .catch(next);
+    .catch(next)
 
   // You could also deny the login request which tells hydra that no one authenticated!
   // hydra.rejectLoginRequest(challenge, {
@@ -115,6 +138,6 @@ router.post('/', csrfProtection, (req, res, next) => {
   //   })
   //   // This will handle any error that happens when making HTTP calls to hydra
   //   .catch(next);
-});
+})
 
 export default router
--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -2,61 +2,66 @@ import express from 'express'
 import url from 'url'
 import urljoin from 'url-join'
 import csrf from 'csurf'
-import {hydraAdmin} from '../config'
+import { hydraAdmin } from '../config'
 
 // Sets up csrf protection
-const csrfProtection = csrf({cookie: true})
+const csrfProtection = csrf({ cookie: true })
 const router = express.Router()
 
 router.get('/', csrfProtection, (req, res, next) => {
   // Parses the URL query
-  const query = url.parse(req.url, true).query;
+  const query = url.parse(req.url, true).query
 
   // The challenge is used to fetch information about the logout request from ORY Hydra.
-  const challenge = String(query.logout_challenge);
+  const challenge = String(query.logout_challenge)
   if (!challenge) {
     next(new Error('Expected a logout challenge to be set but received none.'))
     return
   }
 
-  hydraAdmin.getLogoutRequest(challenge)
+  hydraAdmin
+    .getLogoutRequest(challenge)
     // This will be called if the HTTP request was successful
-    .then(({body}) => {
+    .then(({ body }) => {
       // Here we have access to e.g. response.subject, response.sid, ...
 
       // The most secure way to perform a logout request is by asking the user if he/she really want to log out.
       res.render('logout', {
         csrfToken: req.csrfToken(),
         challenge: challenge,
-        action: urljoin(process.env.BASE_URL || '', '/logout'),
-      });
+        action: urljoin(process.env.BASE_URL || '', '/logout')
+      })
     })
     // This will handle any error that happens when making HTTP calls to hydra
     .catch(next)
-});
+})
 
 router.post('/', csrfProtection, (req, res, next) => {
   // The challenge is now a hidden input field, so let's take it from the request body instead
-  const challenge = req.body.challenge;
+  const challenge = req.body.challenge
 
   if (req.body.submit === 'No') {
-    return hydraAdmin.rejectLogoutRequest(challenge)
-      .then(() => {
-        // The user did not want to log out. Let's redirect him back somewhere or do something else.
-        res.redirect('https://www.ory.sh/');
-      })
-      // This will handle any error that happens when making HTTP calls to hydra
-      .catch(next);
+    return (
+      hydraAdmin
+        .rejectLogoutRequest(challenge)
+        .then(() => {
+          // The user did not want to log out. Let's redirect him back somewhere or do something else.
+          res.redirect('https://www.ory.sh/')
+        })
+        // This will handle any error that happens when making HTTP calls to hydra
+        .catch(next)
+    )
   }
 
   // The user agreed to log out, let's accept the logout request.
-  hydraAdmin.acceptLogoutRequest(challenge)
-    .then(({body}) => {
+  hydraAdmin
+    .acceptLogoutRequest(challenge)
+    .then(({ body }) => {
       // All we need to do now is to redirect the user back to hydra!
-      res.redirect(String(body.redirectTo));
+      res.redirect(String(body.redirectTo))
     })
     // This will handle any error that happens when making HTTP calls to hydra
-    .catch(next);
-});
+    .catch(next)
+})
 
 export default router
