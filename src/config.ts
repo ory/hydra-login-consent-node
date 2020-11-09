050167b74fae4420a07ca92cf615da9d41b5b5d2
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-09 12:02:39 +0100

    feat: add oidc conformity stubs

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
-import {AdminApi} from '@oryd/hydra-client'
+import { AdminApi } from '@oryd/hydra-client'
 
 const hydraAdmin = new AdminApi(process.env.HYDRA_ADMIN_URL)
 if (process.env.MOCK_TLS_TERMINATION) {
   hydraAdmin.defaultHeaders['X-Forwarded-Proto'] = 'https'
 }
 
-export {
-  hydraAdmin
-}
+export { hydraAdmin }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 export default router
