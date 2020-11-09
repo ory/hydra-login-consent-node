43641aecf1437a34035bdbc43618445240b9039c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-09 12:06:21 +0100

    fix: properly set x-forwarded-proto header
    Closes #69

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,14 @@ import { AdminApi } from '@oryd/hydra-client'
 
 const hydraAdmin = new AdminApi(process.env.HYDRA_ADMIN_URL)
 if (process.env.MOCK_TLS_TERMINATION) {
-  hydraAdmin.defaultHeaders['X-Forwarded-Proto'] = 'https'
+  let headers = { 'X-Forwarded-Proto': 'https' }
+  if (hydraAdmin.defaultHeaders) {
+    headers = {
+      ...hydraAdmin.defaultHeaders,
+      ...headers
+    }
+  }
+  hydraAdmin.defaultHeaders = headers
 }
 
 export { hydraAdmin }
