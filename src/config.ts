aa67d44eee0eb00638b757782e2ea90779380e69
Mirco Sanguineti <19243840+msanguineti@users.noreply.github.com>
2020-11-11 11:26:10 +0200

    fix: issue #69

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
-import {AdminApi} from '@oryd/hydra-client'
+import { AdminApi } from '@oryd/hydra-client'
 
-const hydraAdmin = new AdminApi(process.env.HYDRA_ADMIN_URL)
-if (process.env.MOCK_TLS_TERMINATION) {
-  hydraAdmin.defaultHeaders['X-Forwarded-Proto'] = 'https'
-}
+const basePath = process.env.HYDRA_ADMIN_URL
+const baseOptions: Record<string, unknown> = {}
 
-export {
-  hydraAdmin
-}
+if (process.env.MOCK_TLS_TERMINATION)
+  baseOptions.headers = {
+    'X-Forwarded-Proto': 'https',
+  }
+
+export const hydraAdmin = new AdminApi({ basePath, baseOptions })
