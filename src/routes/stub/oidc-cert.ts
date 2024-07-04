c342002f2f0cb477bf0e4b58ad43ab8b16a12099
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-07-04 12:48:31 +0200

    chore: format

--- src/routes/stub/oidc-cert.ts
+++ src/routes/stub/oidc-cert.ts
@@ -1,4 +1,4 @@
-// Copyright © 2023 Ory Corp
+// Copyright © 2024 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
 // This file contains logic which is used when running this application as part of the
